acafa281515d9474284ac734bcea87ec26779d8a
isabellaluballo <95071821+isabellaluballo@users.noreply.github.com>
2022-03-15 20:07:12 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * print_alphabet - Prints alphabets in lowercase
+ *
+ * Retrun: Void
+ *
+ */
+
+void print_alphabet(void)
+{
+
+	int i;
+
+	for (i = 97; i < 123; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+}
